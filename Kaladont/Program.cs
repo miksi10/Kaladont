98e9a5354ddf52c4776a1ee51744d36814f534f1
Mihajlo Stojanovic <Lenovo@DESKTOP-S2B1O5J>
2022-02-07 22:36:15 +0100

    Third

--- Kaladont/Program.cs
+++ Kaladont/Program.cs
@@ -8,6 +8,7 @@ namespace Kaladont
         {
             Console.WriteLine("Second commit to develop branch");
             ////
+            Console.WriteLine("Third commit to develop branch");
         }
     }
 }
