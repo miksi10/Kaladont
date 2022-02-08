9d41970a8976c16335ed4ea6ef108120bd6f3026
Mihajlo Stojanovic <Lenovo@DESKTOP-S2B1O5J>
2022-02-08 23:52:03 +0100

    Added Graph class

---
+++ Kaladont/Graph.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaladont
+{
+    class Graph
+    {
+
+        //adds edge u -> v
+        public static void addEdge(Dictionary<string, LinkedList<string>> adj, string u, string v)
+        {
+            if (adj.ContainsKey(u)) {
+                adj[u].AddLast(v);
+            }
+            else
+            {
+                adj.Add(u, new LinkedList<string>());
+                adj[u].AddLast(v);
+            }
+        }
+
+    }
+}
